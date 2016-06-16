0a7ae6a4853e3c38ba79122412943d65525cb405
Sumanyu Sharma <sumanyu@hotmail.com>
2016-06-16 16:31:04 -0400

    Change the discretization time step instead

--- src/main/scala/core/universe/UniverseConstants.scala
+++ src/main/scala/core/universe/UniverseConstants.scala
@@ -1,10 +1,9 @@
 package core.universe
 
 object UniverseConstants {
-  private val boostedGravitation = 20000
-  val GRAVITATION: Double = 6.67e-11 * boostedGravitation
-  val DISCRETIZED_TIME_STEP: Double = 1e11
+  val GRAVITATION: Double = 6.67e-11
+  val DISCRETIZED_TIME_STEP: Double = 1e11 * 200
   val SOLAR_MASS: Double = 1.98892e30
   val UNIVERSE_RADIUS: Double = 1e18
-  val MIN_MASS = 1e20
+  val MIN_MASS = 1e25
 }
